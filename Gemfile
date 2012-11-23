5e7660d15deaed189de0a44ebea4d24cc4bedb2d
Thomas Metzmacher <kontakt@thomet.de>
2012-11-23 21:58:40 +0100

    add google analytics

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 
 gem 'sinatra'
 gem 'haml'
+gem 'rack-google-analytics'
 
 group :development do
   gem 'shotgun'
--- application.rb
+++ application.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'rack-google-analytics'
+
+use Rack::GoogleAnalytics, :tracker => 'UA-36558630-1'
 
 get '/' do
   haml :index
