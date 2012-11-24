715a3ba4660811341986830b5347e0058f1b493e
Thomas Metzmacher <kontakt@thomet.de>
2012-11-24 23:59:17 +0100

    Use sinatra assetpack and add jquery

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source :rubygems
 
 gem 'sinatra'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
 gem 'haml'
+gem 'sass'
 gem 'rack-google-analytics'
 
 group :development do
--- application.rb
+++ application.rb
@@ -1,10 +1,32 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sass'
 require 'rack-google-analytics'
 
-use Rack::GoogleAnalytics, :tracker => 'UA-36558630-1'
+require 'sinatra/assetpack'
 
-get '/' do
-  haml :index
+class Application < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/javascripts', from: 'assets/javascripts'
+    serve '/stylesheets', from: 'assets/stylesheets'
+    serve '/images',      from: 'assets/images'
+
+    css :application, '/stylesheets/application.css', [
+      '/stylesheets/screen.css'
+    ]
+
+    css_compression :sass
+  }
+
+  # Google Analytics
+  use Rack::GoogleAnalytics, :tracker => 'UA-36558630-1'
+
+  # Routes
+  get '/' do
+    haml :index
+  end
 end
