0fc49ccc387812f79f1137d3c06dbd14ffb9dcf0
Thomas Metzmacher <kontakt@thomet.de>
2012-11-25 13:22:36 +0100

    add font-awesome icons

--- application.rb
+++ application.rb
@@ -76,6 +76,19 @@ class Application < Sinatra::Base
         [e]
       end
     end
+
+    def cc_html(options={}, &blk)
+      attrs = options.map { |(k, v)| " #{h k}='#{h v}'" }.join('')
+      [ "<!--[if lt IE 7 ]> <html#{attrs} class='ie ie6'> <![endif]-->",
+        "<!--[if IE 7 ]>    <html#{attrs} class='ie ie7'> <![endif]-->",
+        "<!--[if IE 8 ]>    <html#{attrs} class='ie ie8'> <![endif]-->",
+        "<!--[if (gte IE 9)|!(IE)]><!--> <html#{attrs}> <!--<![endif]-->",
+        capture_haml(&blk).strip,
+        "</html>"
+      ].join("\n")
+    end
+
+    def h(str); Rack::Utils.escape_html(str); end
   end
 
   # Routes
